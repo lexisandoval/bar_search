90a0046299c9625dddf776cb1145df790b128b20
Lexi Sandoval <lexi.sandoval9@gmail.com>
2021-02-15 22:27:28 +0000

    Scraped data from website

--- lib/bar_search.rb
+++ lib/bar_search.rb
@@ -4,8 +4,12 @@ require_relative "./bar_search/version"
 require_relative "./bar_search/cli"
 require 'colorize'
 
+require 'pry'
+require 'nokogiri'
+require 'open-uri'
+
+
 # module BarSearch
 #   class Error < StandardError; end
 #   # Your code goes here...
 # end
-
--- lib/bar_search/cli.rb
+++ lib/bar_search/cli.rb
@@ -1,16 +1,17 @@
-class BarSearch::CLI 
-    
+class BarSearch::CLI
+
     NEIGH = ["West Loop", "Bronzeville", "Lakeview"]
 
     def call
         puts "\n\nWelcome to Chicago's Bar Search!".light_blue
+        scrape
         menu
         goodbye
     end
 
     def menu
         input = nil
-        
+
         while input != "exit"
 
             puts "\nWhich of the following neighborhoods would you like to browse?"
@@ -27,7 +28,7 @@ class BarSearch::CLI
                 puts "\ninfo for WL".blue
             when "2"
                 puts "\ninfo for BV".blue
-            when "3" 
+            when "3"
                 puts "\ninfo for LV".blue
             else
                 puts "\nI didn't get that. Please enter a number or type \"exit\".".light_magenta
@@ -42,7 +43,22 @@ class BarSearch::CLI
 
 
     def scrape
+      doc = Nokogiri::HTML(open("https://www.theinfatuation.com/chicago/guides/best-bars-in-chicago"))
+      bars = doc.css(".spot-block__title-copy h3").text.strip
+      info = doc.css(".spot-block__description-section p").text.strip
 
+      ## prices for each bar
+      price = []
+      doc.css(".overview-price-rating").each do |bar|
+        price << bar.attribute("data-price").value
+      end
+
+      #temporary link for restaurant address and website
+      link = Nokogiri::HTML(open("https://www.theinfatuation.com/chicago/reviews/guthries-tavern"))
+      address = link.css(".post__sidebar__info__col__block.contact-info .small").first.text
+      website = link.css(".post__sidebar__info__website a").attribute("href").value
+
+    #  binding.pry
 
     end
 end
